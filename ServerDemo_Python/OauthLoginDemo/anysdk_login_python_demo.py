ec2895b6b632ef322160908fab1483d0968a7412
encal <hackit@qq.com>
2016-01-04 14:45:41 +0800

    [python demo]增加 Cocos Play 特殊改动

--- ServerDemo_Python/OauthLoginDemo/anysdk_login_python_demo.py
+++ ServerDemo_Python/OauthLoginDemo/anysdk_login_python_demo.py
@@ -6,7 +6,7 @@ def login_game_xxx(request):
     ret = 'login failed'
 
     private_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-    #loginCheckUrl = 'http://oauth.anysdk.com/api/User/LoginOauth/'
+    #loginCheckUrl = 'http://callback-play.cocos.com/api/User/LoginOauth/'
 
     if request.method == 'POST':
         post_key = request.POST.get('private_key', '')
@@ -14,7 +14,7 @@ def login_game_xxx(request):
             post_data = {}
             post_data = request.POST
 
-            anysdk_conn = httplib.HTTPConnection('oauth.anysdk.com')
+            anysdk_conn = httplib.HTTPConnection('callback-play.cocos.com')
             headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
             post_str = ''
@@ -33,7 +33,9 @@ def login_game_xxx(request):
                 content = response.read()
                 print "any_sdk login result:", content
                 resp_dict = json.loads(content)
-                resp_dict['ext'] = 'game_tag'
+                resp_dict['ext'] = {
+                    "accountID": resp_dict["common"]["uid"]
+                }
                 ret = resp_dict
 
     html = simplejson.dumps(ret, ensure_ascii=False)
